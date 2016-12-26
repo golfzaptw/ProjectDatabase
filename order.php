85b0311cbfaae036dd5ba0bd321e26a0e3720c24
golfzaptw <ptw_golf@hotmail.com>
2016-12-26 20:20:18 +0700

    order table
    ตารางลำดับคิว

--- order.php
+++ order.php
@@ -1,50 +1,86 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" ng-app="app">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  <title>order</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
+      <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>หน้า Order</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  
   <link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
-  <link rel="stylesheet" type="text/css" href="css.css">
-  <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+ 
+
+  <script src="./js/jquery.js"></script>
+  <script type="text/javascript" scr="./js/jquery.js"></script>
+  <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+
+    
 </head>
+
+
+
 
 <style >
   
 .facebook:hover{
   opacity: 0.6;
-  
+
 }
 
 
 
+article {
+    
+    background-color: #FFEC8B;
+    margin-left: 0px;
+    border-left:70px ;
+    padding: 1em;
+    overflow: hidden;
+}
+
+footer {
+    padding: 1em;
+    color: white;
+    background-color: black;
+    clear: left;
+    text-align: center;
+}
+
+
 </style>
+
+
+ 
+
 <body>
 
-    
-    <nav id="scrollingNav" class="navbar navbar-inverse navbar-fixed-top" role= "navigation">
+       
+<nav id="scrollingNav" class="navbar navbar-inverse navbar-fixed-top" role= "navigation">
 <div class="container">
+
+
  
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
+ 
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand">  <img class="circle" src="./image/icon5.png" width="30" height="30"  border-radius="50"/></a>
-      <a class="navbar-brand" href="#">ร้านลุงเพชร</a>
-    </div>
 
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    
+      <a class="navbar-brand">  <img class="circle" src="./image/icon5.png" width="30" height="30"  border-radius="50"/></a>
+      <a class="navbar-brand" href="#">ร้านลุงเพชร </a>
+      
+    </div>
+   
 
 
       
       <ul class="nav navbar-nav navbar-right">
-      <li><a href="index.php">Home</a></li>
+      <li  ><a href="index.php">Home</a></li>
        <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">รายการอาหาร <span class="caret"></span></a>
           <ul class="dropdown-menu">
@@ -80,6 +116,98 @@
 </div>
 
 
+
+
+
+<br><br>
+<div class="">
+  
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    
+  </ol>
+
+      </div>
+
+
+  <center><img src="image/icon10.png" style="width:404px;height:228px;" align="center" ></center>
+    </div>
+    <br>
+<?php
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = '';
+  $dbname = 'admin';
+
+  try {
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+    $sql = $dbh->prepare("SELECT * FROM order_head");
+
+    if($sql->execute()) {
+       $sql->setFetchMode(PDO::FETCH_ASSOC);
+    }
+  }
+  catch(Exception $error) {
+      echo '<p>', $error->getMessage(), '</p>';
+  }
+
+?>
+<div class="container">         
+  <table class="table table-hover">
+  <?php while($row = $sql->fetch()) { ?>
+      <tr>
+        <td><?php echo $row['o_name']; ?></td>
+        <td><?php echo $row['o_addr']; ?></td>
+        <td><?php echo $row['o_phone']; ?></td>
+      </tr>
+      <?php } ?>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Phone</th>
+      </tr>
+    </thead>
+  </table>
+</div>
+
+
+   </div>
+</div>
+</div>
+
+
+</article>
+
+
+
+
+
+
+
+
+<br><br><br>
+
+<div class="container" >
+     
+
+      </div></div></div>
+</div>
+</div>
+
+
+
+
+
+
+
+<!-- ส่วนการแสดง POPUP -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -111,6 +239,15 @@
 
 
 
+<br><br><br><br><br><br><br><br><br><br><br>
+
+
+
+<footer>  </footer>
+
 
 
 </body>
+</html>
+
+
